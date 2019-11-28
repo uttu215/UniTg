84c816a5a3d6e5aaac35596a5b5571928b960358
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2019-11-28 14:36:16 +0530

    Update delpfp.py

--- stdplugins/delpfp.py
+++ stdplugins/delpfp.py
@@ -9,21 +9,21 @@ import asyncio
 import shutil
 
 @borg.on(admin_cmd("delpfp ?(.*)"))
-async def delpfpcmd(self, message):
+async def delpfpcmd(event):
         
 
-        args = utils.get_args(message)
+        args = utils.get_args(event)
         if not args:
-            return await utils.answer(message, _("<b>Please specify number of profile pics to remove.</b>"))
+            return await utils.answer(event, _("<b>Please specify number of profile pics to remove.</b>"))
         if args[0].lower() == "unlimited":
             pfps_count = None
         else:
             try:
                 pfps_count = int(args[0])
             except ValueError:
-                return await utils.answer(message, _("<b>Wrong amount of pfps.</b>"))
+                return await utils.answer(event, _("<b>Wrong amount of pfps.</b>"))
             if pfps_count <= 0:
-                return await utils.answer(message, _("<b>Please provide positive number of"
+                return await utils.answer(event, _("<b>Please provide positive number of"
                                                      + " profile pictures to remove.</b>"))
 
         await self.client(functions.photos.DeletePhotosRequest(await self.client.get_profile_photos("me",
@@ -32,4 +32,4 @@ async def delpfpcmd(self, message):
         if pfps_count is None:
             pfps_count = _("all")
         await self.allmodules.log("delpfp")
-        await utils.answer(message, _("<b>Removed </b><code>{}</code><b> profile pic(s).</b>".format(str(pfps_count))))
+        await utils.answer(event, _("<b>Removed </b><code>{}</code><b> profile pic(s).</b>".format(str(pfps_count))))
