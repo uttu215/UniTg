2c37db18f388b7b02ebf789b03548a02e4b27e8a
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2019-12-05 00:02:34 +0530

    Update fun.py

--- stdplugins/fun.py
+++ stdplugins/fun.py
@@ -7,9 +7,20 @@ METOOSTR = [
     "`Haha yes, me too`",
     "`Same lol`",
     "`Me irl`",
+    "`HA me bhi bhai bsdk!`",
     "`Same here`",
     "`Haha yes`",
     "`Same pinch bsdk`",
+]
+NOOBSTR = [
+    "`YOU PRO NIMBA DONT MESS WIDH MEH`",
+    "`Haha yes`",
+    "`NOOB NIMBA TRYING TO BE FAMOUS KEK`",
+    "`Sometimes one middle finger isn’t enough to let someone know how you feel. That’s why you have two hands`",
+    "`Some Nimbas need to open their small minds instead of their big mouths`",
+    "`UH DONT KNOW MEH SO STAY AWAY LAWDE`",
+    "`Kysa kysaaaa haaan? Phir MAAR nhi Khayega tu?`",
+    "`Zikr Jinka hota hai galiyo meh woh bhosdika ajj paya gya naliyo me`",
 ]
 RUNSREACTS = [
     "`Runs to Thanos`",
@@ -26,10 +37,15 @@ RUNSREACTS = [
 ]
 RAPE_STRINGS = [
      "`Rape Done Drink The Cum`",
+     "`EK baat yaad rkhio, Chut ka Chakkar matlab maut se takkar`",
      "`The user has been successfully raped`",
      "`Dekho Bhaiyya esa hai! Izzat bachailo apni warna Gaand maar lenge tumhari`",
      "`Relax your Rear, ders nothing to fear,The Rape train is finally here`",
      "`Rape coming... Raped! haha 😆`",
+     "`Kitni baar Rape krvyega mujhse?`",
+     "`Tu Randi hai Sabko pta hai😂`",
+     "`Don't rape too much bossdk, else problem....`",
+     "`Tu sasti rendi hai Sabko pta hai😂`",
      "`Lodu Andha hai kya Yaha tera rape ho raha hai aur tu abhi tak yahi gaand mara raha hai lulz`",
 ] 
 ABUSE_STRINGS = [
@@ -37,31 +53,41 @@ ABUSE_STRINGS = [
 	   "`Gaandu`",
 	   "`Chutiya he rah jaye ga`",
 	   "`Ja be Gaandu`",
-	   "`Ma ka Bhodsa madharchod`",
+	   "`Ma ka Bharosa madharchod`",
 	   "`mml`",
-	   "`You MotherFukcer`",
-           "`You Betichod`",
-           "`you are lodu no.1`"
+	   "`You MotherFeker`",
 	   "`Muh Me Lega Bhosdike ?`"
+	   "`Kro Gandu giri kam nhi toh Gand Maar lenge tumhari hum😂`",
+           "`Suno Lodu Jyda muh na chalo be muh me lawda pel Diyaa jayega`",
+           "`Sharam aagyi toh aakhe juka lijia land me dam nhi hai apke toh Shilajit kha lijia`",
+           "`Kahe Rahiman Kaviraaj C**t Ki Mahima Aisi,L**d Murjha Jaaye Par Ch**t Waisi Ki Waisi`",
+           "`Chudakkad Raand Ki Ch**T Mein Pele L*Nd Kabeer, Par Aisa Bhi Kya Choda Ki Ban Gaye Fakeer`",
 ]
 GEY_STRINGS = [
-     "`you gey bsdk`",
+          "`you gey bsdk`",
      "`you gey`",
      "`you gey in the house`",
      "`you chakka`",
+     "`Bhago BC! Chakka aya`",
      "`you gey gey gey gey gey gey gey gey`",
      "`you gey go away`",
 ]
 PRO_STRINGS = [
      "`This gey is pro as phack.`",
      "`Pros here -_- Time to Leave`",
+     "`Proness Lebel: 6969696969`",
+     "`Itna pro banda dekhlia bc, ab to marna hoga.`",
+     "`U iz pro but i iz ur DAD, KeK`",
+     "`What are you Bsdk? Human or Gawd(+_+)`",
+     "`Aye pro,ek baat yaad rakhna, Agar Bharosa khud par ho to ksi ki chut tumhari kamzori nahi bnskti.`",
+
 ]
 INSULT_STRINGS = [ 
-    "`Owww ... Such a stupid idiot.`",
-    "`BC.. Gaand na fulao, maa chod denge tumhari`",
+    
+     "`Owww ... Such a stupid idiot.`",
     "`Don't drink and type.`",
     "`Command not found. Just like your brain.`",
-    "`Bot rule 544 section 9 prevents me from replying to stupid humans like you.`",
+    "`Bot rule 420 section 69 prevents me from replying to stupid nubfuks like you.`",
     "`Sorry, we do not sell brains.`",
     "`Believe me you are not normal.`",
     "`I bet your brain feels as good as new, seeing that you never use it.`",
@@ -77,16 +103,22 @@ INSULT_STRINGS = [
     "`I'm sorry I hurt your feelings when I called you stupid. I thought you already knew that.`",
     "`You should try tasting cyanide.`",
     "`You should try sleeping forever.`",
+    "`Sharam kar bsdwale,kitni bkchodi deta.`",
+    "`Chup Madarhox, bilkul chup..`",
+    "`Me zindagi me chunotiyo se jyda inn jese Chutiyo se pareshaan hu.`",
     "`Pick up a gun and shoot yourself.`",
     "`Try bathing with Hydrochloric Acid instead of water.`",
     "`Go Green! Stop inhaling Oxygen.`",
     "`God was searching for you. You should leave to meet him.`",
     "`You should Volunteer for target in an firing range.`",
     "`Try playing catch and throw with RDX its fun.`",
+    "`Jaana chodu chad jake land chaat`",
+    "`Yaar ajab tere nkhare,gazab tera style hain, gand dhone ki tameez nahi, haath main mobile hai`",
     "`People like you are the reason we have middle fingers.`",
     "`When your mom dropped you off at the school, she got a ticket for littering.`",
     "`You’re so ugly that when you cry, the tears roll down the back of your head…just to avoid your face.`",
     "`If you’re talking behind my back then you’re in a perfect position to kiss my a**!.`",
+
 ]
 # ===========================================
                           
@@ -158,4 +190,15 @@ async def _(event):
     bro = random.randint(0, len(GEY_STRINGS) - 1)    
     input_str = event.pattern_match.group(1)
     reply_text = GEY_STRINGS[bro]
-    await event.edit(reply_text)                    
+    await event.edit(reply_text)       
+	
+	
+	
+@borg.on(admin_cmd("noob ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+         return
+    bro = random.randint(0, len(NOOBSTR) - 1)    
+    input_str = event.pattern_match.group(1)
+    reply_text = NOOBSTR[bro]
+    await event.edit(reply_text)
