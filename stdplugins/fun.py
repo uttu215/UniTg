25fc46b02250ba7be77c51cb98c77c21aba1bcb2
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2019-12-05 01:09:19 +0530

    Update fun.py

--- stdplugins/fun.py
+++ stdplugins/fun.py
@@ -249,7 +249,7 @@ async def _(event):
     animation_interval = 0
     
 
-    animation_ttl = range(0, 80)
+    animation_ttl = range(0, 103)
 
     input_str ="oof"
 
@@ -277,5 +277,5 @@ async def _(event):
         for i in animation_ttl:
 
 
-            await event.edit(animation_chars[i % 80])
+            await event.edit(animation_chars[i % 104])
 
