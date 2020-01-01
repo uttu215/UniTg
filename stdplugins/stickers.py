447d97d90126d75f68c275febd3f2148791b2d39
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-01-01 12:11:56 +0530

    Update stickers.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -27,7 +27,24 @@ from telethon.tl.types import (
     MessageMediaPhoto
 )
 from uniborg.util import admin_cmd
-
+KANGING_STR = [
+    "Using Witchery to kang this sticker...",
+    "Plagiarising hehe...",
+    "Inviting this sticker over to my pack...",
+    "Kanging this sticker...",
+    "Hey that's a nice sticker!\
+    \nMind if I kang?!..",
+    "hehe me stel ur stikér\
+    \nhehe.",
+    "Ay look over there (☉｡☉)!→\
+    \nWhile I kang this...",
+    "Roses are red \
+    \nviolets are blue, \
+    \nkanging this sticker \
+    \nso my pacc looks cool",
+    "Imprisoning this sticker...",
+    "Mr.Steal Your Sticker is stealing this sticker... ",
+]
 
 @borg.on(admin_cmd(pattern="kangsticker ?(.*)"))
 async def _(event):
@@ -65,7 +82,7 @@ async def _(event):
             sticker.seek(0)
             uploaded_sticker = await borg.upload_file(sticker, file_name=file_ext_ns_ion)
 
-    await event.edit("Processing this sticker. Please Wait!")
+    await event.edit(random.choice(KANGING_STR))
 
     async with borg.conversation("@Stickers") as bot_conv:
         now = datetime.datetime.now()
