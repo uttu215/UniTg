049d2949096d9e43e8396472c55858958e69dd91
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-01-09 19:16:57 +0530

    Update bigf.py

--- stdplugins/bigf.py
+++ stdplugins/bigf.py
@@ -19,5 +19,5 @@ async def _(event):
                  logger.debug(r)
                  if r == 0:
                      await event.edit("┏━━━┓\n┃┏━━┛\n┃┗━━┓\n┃┏━━┛\n┃┃\n┗┛")
-                else r == 1:
-                      await event.edit("╭━━━╮\n┃╭━━╯\n┃╰━━╮\n┃╭━━╯\n┃┃\n╰╯")
+                 else r == 1:
+                       await event.edit("╭━━━╮\n┃╭━━╯\n┃╰━━╮\n┃╭━━╯\n┃┃\n╰╯")
