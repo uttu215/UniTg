cc37c49f03457111e394811f62305cf6dfcfaf0e
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-01-10 12:39:53 +0530

    Update pmpermit.py

--- stdplugins/pmpermit.py
+++ stdplugins/pmpermit.py
@@ -52,7 +52,7 @@ async def monito_p_m_s(event):
             borg.storage.PREV_REPLY_MESSAGE[chat.id] = r
 
 
-@borg.on(admin_cmd("approvepm ?(.*)"))
+@borg.on(admin_cmd("pm ?(.*)"))
 async def approve_p_m(event):
     if event.fwd_from:
         return
@@ -72,7 +72,7 @@ async def approve_p_m(event):
                 await event.delete()
 
 
-@borg.on(admin_cmd("blockpm ?(.*)"))
+@borg.on(admin_cmd("dis ?(.*)"))
 async def approve_p_m(event):
     if event.fwd_from:
         return
