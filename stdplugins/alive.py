d0f91bf64b771be06cbd4d62306aecd8dca45dd0
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-01-10 13:39:55 +0530

    Update alive.py

--- stdplugins/alive.py
+++ stdplugins/alive.py
@@ -8,23 +8,14 @@ from uniborg.util import admin_cmd
 
 DEFAULTUSER = Config.ALIVE_NAME
     
-@borg.on(admin_cmd("alive"))
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
-    if event.fwd_from:
-        return
-    
-    chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        mentions += f""
-    reply_message = None
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        await reply_message.reply(mentions)
-    else:
-        await event.reply("`"
+           input_str = event.pattern_match.group(1)
+           if input_str == "alive":
+                 await event.reply("`"
                      "My bot is running \n\n"
                      f"Telethon version: 1.10 \n"
                      f"Python: 3.8 \n"
                      f"User: {DEFAULTUSER}"
                      "`")
-    await event.delete()
+                 await event.delete()
