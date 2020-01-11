a1db0e30eb95f32f6f284215e50de037a2dfeb8a
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-01-11 23:24:23 +0530

    Update shoot.py

--- stdplugins/shoot.py
+++ stdplugins/shoot.py
@@ -3,13 +3,25 @@ from telethon import events, functions, __version__
 from uniborg.util import admin_cmd
 import asyncio
 
+KILLSTR = [
+    "AWM",
+    "M24",
+    "Kar98",
+    "AKM",
+    "M416",
+    "UZI",
+    "UMP9",
+    "Knife",
+    "Diamond Sword",
+]    
+KILL = random.choice(KILLSTR)
 
-@borg.on(admin_cmd(pattern="shoot$ ?(.*)", allow_sudo=True))  # pylint:disable=E0602
+@borg.on(admin_cmd(pattern="kill$ ?(.*)", allow_sudo=True))  # pylint:disable=E0602
 async def killing (killed):
     """ Dont Kill Too much -_-"""
     if not killed.text[0].isalpha() and killed.text[0] not in ("/", "#", "@", "!"):
         if await killed.get_reply_message():
             await killed.edit(
-                "Targeted user killed by Headshot 😈......\n"
+                f"Targeted user killed with {KILL} 😈......\n"
   "#Sad_Reacts_Onli\n"
             )
