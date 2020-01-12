105b7c99af95dfb54ddf9d3e864827ec65ec46dc
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-01-12 10:17:11 +0530

    Update and rename F.py to f.py

--- stdplugins/f.py
+++ stdplugins/f.py
@@ -1,3 +1,5 @@
+""" .f emoji or chracter
+
 from telethon import events
 import asyncio
 import os
@@ -5,7 +7,7 @@ import sys
 from uniborg import util
 
 
-@borg.on(util.admin_cmd("f ?(.*)"))
+@borg.on(util.admin_cmd("fk ?(.*)"))
 async def payf(event):
     paytext=event.pattern_match.group(1)
     pay = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(paytext*8, paytext*8, paytext*2, paytext*2, paytext*2, paytext*6, paytext*6, paytext*2, paytext*2, paytext*2, paytext*2, paytext*2)
