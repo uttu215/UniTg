9b67b7cc4dd8a5144e042f9a0e0ecdb980cf0ff4
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-04-05 20:08:09 +0530

    Update alive.py

--- stdplugins/alive.py
+++ stdplugins/alive.py
@@ -8,7 +8,7 @@ from uniborg.util import admin_cmd
 
 DEFAULTUSER = Config.ALIVE_NAME
     
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True, allow_sudo=True))
 async def _(event):
            input_str = event.pattern_match.group(1)
            if input_str == "alive":
