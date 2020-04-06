2dae6d5a17ad7839cea90e5612ac6dfdb67cb8e0
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-04-06 23:15:22 +0530

    Update purge.py

--- stdplugins/purge.py
+++ stdplugins/purge.py
@@ -32,3 +32,22 @@ async def _(event):
             await event.delete()
         else:
             await event.edit("**PURGE** Failed!")
+
+@borg.on(events.NewMessage(pattern="\.purgeme", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    i = 1
+    shiiinabot = "\u2060"
+    for i in range(4006):
+        shiiinabot += "\u2060"
+    await event.edit(shiiinabot)
+    await event.delete()
+    async for message in borg.iter_messages(event.chat_id, from_user="me"):
+        i = i + 1
+        try:
+            #  await message.edit(shiiinabot)
+            await message.delete()
+        except:
+            pass
+        await asyncio.sleep(10)
