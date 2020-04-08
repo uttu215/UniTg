e76d53c556b0eba26943d226f821ab6057882761
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-04-08 13:59:19 +0530

    Create alive1.py

---
+++ stdplugins/alive1.py
@@ -0,0 +1,21 @@
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from uniborg.util import admin_cmd
+
+DEFAULTUSER = Config.ALIVE_NAME
+@borg.on(admin_cmd("alive"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = f"My bot is running.\n\nTelethon version: 1.11.3.\nPython: 3.8.2.\nUser: {DEFAULTUSER}."
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()                   
