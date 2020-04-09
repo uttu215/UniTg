7905706be10f558c1a690bedb6543b1c52b0ec9b
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-04-09 20:50:53 +0530

    Update speedtest.py

--- stdplugins/speedtest.py
+++ stdplugins/speedtest.py
@@ -13,15 +13,15 @@ async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    as_text = False
-    as_document = True
+    as_text = True
+    as_document = False
     if input_str == "image":
         as_document = False
     elif input_str == "file":
         as_document = True
     elif input_str == "text":
         as_text = True
-    await event.edit("Calculating my internet speed. Please wait!")
+    await event.edit("Running speed test!")
     start = datetime.now()
     s = speedtest.Speedtest()
     s.get_best_server()
@@ -43,12 +43,12 @@ async def _(event):
         response = s.results.share()
         speedtest_image = response
         if as_text:
-            await event.edit("""**SpeedTest** completed in {} seconds
+            await event.edit("""**SpeedTest Results**:\n\n
 Download: {}
 Upload: {}
 Ping: {}
 Internet Service Provider: {}
-ISP Rating: {}""".format(ms, convert_from_bytes(download_speed), convert_from_bytes(upload_speed), ping_time, i_s_p, i_s_p_rating))
+ISP Rating: {}""".format(convert_from_bytes(download_speed), convert_from_bytes(upload_speed), ping_time, i_s_p, i_s_p_rating))
         else:
             await borg.send_file(
                 event.chat_id,
