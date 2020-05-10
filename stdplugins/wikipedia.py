505f752f24007e5e28337ae8517855f72d0469e6
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-05-10 18:00:44 +0530

    Update wikipedia.py

--- stdplugins/wikipedia.py
+++ stdplugins/wikipedia.py
@@ -2,22 +2,37 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """WikiPedia.ORG
-Syntax: .wikipedia Query"""
+Syntax: .wiki Query"""
 from telethon import events
 import wikipedia
+from wikipedia.exceptions import DisambiguationError, PageError
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("wikipedia (.*)"))
-async def _(event):
-    if event.fwd_from:
+@borg.on(admin_cmd("wiki (.*)"))
+async def wiki(wiki_q):
+    """ For .wiki command, fetch content from Wikipedia. """
+    match = wiki_q.pattern_match.group(1)
+    try:
+        summary(match)
+    except DisambiguationError as error:
+        await wiki_q.edit(f"Disambiguated page found.\n\n{error}")
         return
-    await event.edit("Processing ...")
-    input_str = event.pattern_match.group(1)
-    result = ""
-    results = wikipedia.search(input_str)
-    for s in results:
-        page = wikipedia.page(s)
-        url = page.url
-        result += f"> [{s}]({url}) \n"
-    await event.edit("WikiPedia **Search**: {} \n\n **Result**: \n\n{}".format(input_str, result))
+    except PageError as pageerror:
+        await wiki_q.edit(f"Page not found.\n\n{pageerror}")
+        return
+    result = summary(match)
+    if len(result) >= 4096:
+        file = open("output.txt", "w+")
+        file.write(result)
+        file.close()
+        await wiki_q.client.send_file(
+            wiki_q.chat_id,
+            "output.txt",
+            reply_to=wiki_q.id,
+            caption="`Output too large, sending as file`",
+        )
+        if os.path.exists("output.txt"):
+            os.remove("output.txt")
+        return
+    await wiki_q.edit("**Search:**\n`" + match + "`\n\n**Result:**\n" + result)
