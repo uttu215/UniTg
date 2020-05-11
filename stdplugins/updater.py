dfb18880cd9ec7d7571bbaa910902b4c08eba1fe
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-05-11 11:06:29 +0530

    Update updater.py

--- stdplugins/updater.py
+++ stdplugins/updater.py
@@ -56,7 +56,8 @@ async def upstream(ups):
         txt = "`Oops.. Updater cannot continue due to "
         txt += "some problems occured`\n\n**LOGTRACE:**\n"
         
-        repo = git.Repo(search_parent_directories=True)
+        repo = Repo(self.rorepo.working_tree_dir)
+        assert not repo.bare
     except NoSuchPathError as error:
         await ups.edit(f'{txt}\n`directory {error} is not found`')
         repo.__del__()
