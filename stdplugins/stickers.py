2d2511a7556907a64c1d4a12c0c8b25b992421b4
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-05-11 10:52:20 +0530

    Update stickers.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -249,7 +249,7 @@ async def kang(args):
                 # Ensure user doesn't get spamming notifications
                 await borg.send_read_acknowledge(conv.chat_id)
 
-        await args.edit(f"`Sticker added to [pack](t.me/addstickers/{packname})", parse_mode='md')
+        await args.edit(f"`Sticker added to [pack](t.me/addstickers/{packname})`", parse_mode='md')
 
 
 async def resize_photo(photo):
