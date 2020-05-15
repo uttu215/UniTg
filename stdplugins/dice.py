b1c3391d0e1e74117f5063e8a3661d415cc2be2a
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-05-15 18:28:56 +0530

    Update dice.py

--- stdplugins/dice.py
+++ stdplugins/dice.py
@@ -33,6 +33,8 @@ async def _(event):
     if input_str:
         try:
             required_number = int(input_str)
+            if required_number >=7:
+                  required_number = 6
             while not r.media.value == required_number:
                 await r.delete()
                 r = await event.reply(file=InputMediaDice("🎯"))
