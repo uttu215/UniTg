ef145f864ddcde272983e51f72506856e7336aa1
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-05-15 18:05:06 +0530

    Create dice.py

---
+++ stdplugins/dice.py
@@ -0,0 +1,57 @@
+"""@RollADie
+Syntax: .dice, .dart, .ball"""
+
+from telethon.tl.types import InputMediaDice
+from uniborg.util import admin_cmd
+
+
+@borg.on(admin_cmd(pattern="dice ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.delete()
+    r = await event.reply(file=InputMediaDice("🎲"))
+    if input_str:
+        try:
+            required_number = int(input_str)
+            while not r.media.value == required_number:
+                await r.delete()
+                r = await event.reply(file=InputMediaDice("🎲"))
+        except:
+            pass
+
+
+
+@borg.on(admin_cmd(pattern="dart ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.delete()
+    r = await event.reply(file=InputMediaDice("🎯"))
+    if input_str:
+        try:
+            required_number = int(input_str)
+            while not r.media.value == required_number:
+                await r.delete()
+                r = await event.reply(file=InputMediaDice("🎯"))
+        except:
+            pass
+
+
+@borg.on(admin_cmd(pattern="ball ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.delete()
+    r = await event.reply(file=InputMediaDice("🏀"))
+    if input_str:
+        try:
+            required_number = int(input_str)
+            while not r.media.value == required_number:
+                await r.delete()
+                r = await event.reply(file=InputMediaDice("🏀"))
+        except:
+            pass
